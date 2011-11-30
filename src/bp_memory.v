0a1ea630daba05002f075da3f08ad84662190f9c
smithre5@muohio.edu <smithre5@muohio.edu@8aa3a851-456d-35c6-071c-23e48d727d0a>
2011-11-30 17:01:13 +0000

    @ Minor update.
    ! src/bp_memory.v: Converted tabs to spaces.

--- src/bp_memory.v
+++ src/bp_memory.v
@@ -51,17 +51,17 @@
 
 
 module bp_memory(
-	//-- module interface ----------------------------------------------------//
-	input				clock, reset,
-	input [17:0]		address,
-	input [15:0]		data_i,
-	output reg [15:0]	data_o,
-	input 				we,
-	
-	//-- chip interface ------------------------------------------------------//
-	output reg [17:0] 	mem_addr,
-	inout [15:0]  		mem_q,
-	output reg 			mem_nce, mem_nwe, mem_noe, mem_nub, mem_nlb
+    //-- module interface ----------------------------------------------------//
+    input               clock, reset,
+    input [17:0]        address,
+    input [15:0]        data_i,
+    output reg [15:0]   data_o,
+    input               we,
+    
+    //-- chip interface ------------------------------------------------------//
+    output reg [17:0]   mem_addr,
+    inout [15:0]        mem_q,
+    output reg          mem_nce, mem_nwe, mem_noe, mem_nub, mem_nlb
 );
 
 
@@ -73,96 +73,96 @@ assign mem_q = (mem_nwe == `LO) ? data_i : 16'hzzzz;
 
 //-- memory control ----------------------------------------------------------//
 
-parameter S_READY 		= 2'd0,
-          S_READ_CONT	= 2'd1,
-          S_READ_DONE	= 2'd2,
-          S_WRITE 		= 2'd3;
+parameter S_READY       = 2'd0,
+          S_READ_CONT   = 2'd1,
+          S_READ_DONE   = 2'd2,
+          S_WRITE       = 2'd3;
 
 reg [1:0] S, NS;
 
 always @(posedge clock or negedge reset) begin
-	if (~reset) begin
-		S <= S_READY;
-	end
-	else begin
-		S <= NS;
-	end	
+    if (~reset) begin
+        S <= S_READY;
+    end
+    else begin
+        S <= NS;
+    end 
 end
 
 always @(*) begin
-	case (S)
-		default: begin
-			NS <= S_READY;
-		end
-		
-		S_READY: begin
-			NS <= S_READ_CONT;
-		
-			if (we) begin
-				NS <= S_WRITE;
-			end
-		end
-		
-		S_READ_CONT: begin
-			NS <= S_READ_DONE;
-		end
-		
-		S_READ_DONE: begin
-			NS <= S_READY;
-		end
-		
-		S_WRITE: begin
-			NS <= S_READY;
-		end
-	endcase
+    case (S)
+        default: begin
+            NS <= S_READY;
+        end
+        
+        S_READY: begin
+            NS <= S_READ_CONT;
+        
+            if (we) begin
+                NS <= S_WRITE;
+            end
+        end
+        
+        S_READ_CONT: begin
+            NS <= S_READ_DONE;
+        end
+        
+        S_READ_DONE: begin
+            NS <= S_READY;
+        end
+        
+        S_WRITE: begin
+            NS <= S_READY;
+        end
+    endcase
 end
 
 always @(posedge clock) begin
-	case (S)
-		default: begin 
-		end
-	
-		// Ready:
-		// 		Chip selected.
-		//		All controls off.
-		S_READY: begin
-			mem_nce <= `LO;
-			mem_nwe <= `HI;
-			mem_noe <= `HI;
-			mem_nub <= `HI;
-			mem_nlb <= `HI;
-		end
-		
-		// Read, Continuous:
-		//		Output enabled, read from upper and lower bytes.
-		//		Drive current address.
-		S_READ_CONT: begin
-			mem_noe <= `LO;
-			mem_nub <= `LO;
-			mem_nlb <= `LO;
-			
-			mem_addr <= address;
-		end
-		
-		// Read, Done:
-		//		Read data on data bus.
-		S_READ_DONE: begin
-			data_o <= mem_q;
-		end
-		
-		// Write:
-		//		Write enabled, output disabled, write to upper and lower bytes.
-		//		Drive current address.
-		//		Data written handled by continuous assignment under signals.
-		S_WRITE: begin
-			mem_nwe <= `LO;
-			mem_noe <= `HI;
-			mem_nub <= `LO;
-			mem_nlb <= `LO;
-			
-			mem_addr <= address;
-		end
-	endcase
+    case (S)
+        default: begin 
+        end
+    
+        // Ready:
+        //      Chip selected.
+        //      All controls off.
+        S_READY: begin
+            mem_nce <= `LO;
+            mem_nwe <= `HI;
+            mem_noe <= `HI;
+            mem_nub <= `HI;
+            mem_nlb <= `HI;
+        end
+        
+        // Read, Continuous:
+        //      Output enabled, read from upper and lower bytes.
+        //      Drive current address.
+        S_READ_CONT: begin
+            mem_noe <= `LO;
+            mem_nub <= `LO;
+            mem_nlb <= `LO;
+            
+            mem_addr <= address;
+        end
+        
+        // Read, Done:
+        //      Read data on data bus.
+        S_READ_DONE: begin
+            data_o <= mem_q;
+        end
+        
+        // Write:
+        //      Write enabled, output disabled, write to upper and lower bytes.
+        //      Drive current address.
+        //      Data written handled by continuous assignment under signals.
+        S_WRITE: begin
+            mem_nwe <= `LO;
+            mem_noe <= `HI;
+            mem_nub <= `LO;
+            mem_nlb <= `LO;
+            
+            mem_addr <= address;
+        end
+    endcase
 end
 
 endmodule
